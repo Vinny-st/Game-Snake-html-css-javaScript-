34db510a343c0d60d66680782d0eff9fb0406774
vinnyST <vinnycert@gmail.com>
2022-03-18 21:42:05 -0400

    definir movimento

--- script.js
+++ script.js
@@ -6,18 +6,42 @@ snake [0] = { /* */
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBG() { /*funcao que vai desenhar e definir o cor no canvas*/
     context.fillStyle = "lightgreen"; /* fillstyle trabalha com o stilo do canvas,*/
     context.fillRect(0, 0, 16 * box, 16 * box); /*fill react desenha o retangulo onde vai rodar jogo*/
 }
+
 function criarCobrinha(){ /* for percorre todo o array e vai incrementar*/
 /*vai pintar e pintar tamanho certo*/
     for(i=0; i < snake.length; i++){
-    context.fillStyle = "green";
+    context.fillStyle = "black";
     context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -=box;
+    if(direction == "down") snake +=box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);
